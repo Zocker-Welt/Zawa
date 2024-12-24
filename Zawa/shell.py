44caff2163b62b7f77b0416d63082a431dad5d35
Zocker Welt <166701868+Zocker-Welt@users.noreply.github.com>
2024-12-24 12:40:06 +0300

    time

--- Zawa/shell.py
+++ Zawa/shell.py
@@ -1,3 +1,5 @@
+import time
+
 from interpreter import Interpreter
 from compiler import Compiler
 
@@ -5,18 +7,25 @@ from compiler import Compiler
 while True:
     inp = input(">> ").split()
     if inp[0] == "run":
+        Time = time.time()
         file = inp[1]
         with open(file, "r") as f:
             data = f.readlines()
         language = Interpreter(data)
         language.run()
+        Time = time.time() - Time
     if inp[0] == "compile":
+        Time = time.time()
         file1 = inp[1]
         file2 = inp[2]
         with open(file1, "r") as f:
             data = f.readlines()
             compiler = Compiler(data, file2)
             compiler.compile()
+        Time = time.time() - Time
+        
+    if inp[0] == "time":
+        print(Time)
 
     if inp[0] == "exit":
         exit()
